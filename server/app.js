8f3186565384f9b0d3ee51ae402a6e0198cb482a
Wladek <wladek.developer@gmail.com>
2025-05-14 15:19:42 +0200

    feat: add test mongodb script

--- server/app.js
+++ server/app.js
@@ -1,10 +1,25 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const MongoClient = require("mongodb").MongoClient
+
+MongoClient.connect("mongodb://localhost:27017/", (err, client) => {
+  if (err) throw err
+
+  const db = client.db("myNewDB")
+
+  db.collection("myNewDBtestCollection").find().toArray((err, result) => {
+    if (err) throw err
+
+    app.get("/", (req, res) => {
+      res.send(result)
+    })
+
+    console.log(result)
+  })
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
