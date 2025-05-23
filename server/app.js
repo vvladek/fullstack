31e1b8514308b0dd100884d413d14e02eaa79a28
Wladek <wladek.developer@gmail.com>
2025-05-23 14:30:34 +0200

    feat: add script to send client page from server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 import express from "express"
 import cors from "cors"
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { TodosRouter } from "./routes/todos.router.js"
 import { initMongoConnection } from "./mongo/mongo.js"
 import { expressServerPort } from "./config/constants.js"
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 class App {
@@ -16,7 +21,11 @@ class App {
     await initMongoConnection()
     this.app.use(cors())
     this.app.use(express.json())
+    this.app.use(express.static(path.join(__dirname, '../client')));
     this.todosRouter.setRoutes()
+    this.app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/index.html'));
+    });
     this.app.listen(expressServerPort, () => console.log(`🚀 Server running on port ${expressServerPort}`))
   }
 }
