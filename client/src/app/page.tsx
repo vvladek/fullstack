7572bcee396995c02de301cc944222aa896502f3
Wladek <wladek.developer@gmail.com>
2025-05-29 19:16:45 +0200

    fix: change / page error handler

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,23 +3,22 @@ import styles from "./page.module.css"
 
 export default async function Home() {
 
-  // const data = await fetch("http://localhost:3000/api/users", { method: "GET" })
-  // if (!data) return(
-  //   <>Hello world</>
-  // )
-  // const users = await data.json()
+  const data = await fetch("http://localhost:3000/api/users", { method: "GET" })
+  if (!data.ok) return(
+    <>Hello world</>
+  )
+  const users = await data.json()
 
-  // if (!users) return(
-  //   <>Hello without users</>
-  // )
+  if (!users) return(
+    <>Hello without users</>
+  )
 
   
 
   return (
     <div className={styles.page}>
       <p>
-        {/* {`${JSON.stringify(users)}`} */}
-        Hello world!
+        {`${JSON.stringify(users)}`}
       </p>     
     </div>
   )
