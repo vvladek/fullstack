9a8085eae5fcb2e3dc24e1e6adc21bba061d3db2
Wladek <wladek.developer@gmail.com>
2025-05-29 15:15:15 +0200

    fix: remove fetchdata in /

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,22 +3,23 @@ import styles from "./page.module.css"
 
 export default async function Home() {
 
-  const data = await fetch("http://localhost:3000/api/users", { method: "GET" })
-  if (!data) return(
-    <>Hello world</>
-  )
-  const users = await data.json()
+  // const data = await fetch("http://localhost:3000/api/users", { method: "GET" })
+  // if (!data) return(
+  //   <>Hello world</>
+  // )
+  // const users = await data.json()
 
-  if (!users) return(
-    <>Hello without users</>
-  )
+  // if (!users) return(
+  //   <>Hello without users</>
+  // )
 
   
 
   return (
     <div className={styles.page}>
       <p>
-        {`${JSON.stringify(users)}`}
+        {/* {`${JSON.stringify(users)}`} */}
+        Hello world!
       </p>     
     </div>
   )
